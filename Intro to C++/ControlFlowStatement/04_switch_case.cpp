a2e9cdc13fc98be23e343f82894342d394a4c313
shiivankiit <shivankit7557@gmail.com>
2025-11-14 21:25:28 +0530

    Switch case

---
+++ Intro to C++/ControlFlowStatement/04_switch_case.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char grade;
+    cout << "Enter your grade" << endl;
+    cin >> grade;
+
+    switch (grade)
+    {
+    case 'A':
+        cout << "Your marks will be in range of 90 to 100" << endl;
+        break;
+    case 'B':
+        cout << "Your marks will be in range of 80 to 90" << endl;
+        break;
+    case 'C':
+        cout << "Your marks will be in range of 70 to 80" << endl;
+        break;
+    case 'D':
+        cout << "Your marks will be in range of 60 to 70" << endl;
+        break;
+    case 'E':
+        cout << "your marks will be in range of 0 to 60" << endl;
+        break;
+    }
+}
